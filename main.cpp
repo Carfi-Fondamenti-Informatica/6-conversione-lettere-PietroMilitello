436c39949ffb0c278d121d9e709acbd3707915c3
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-11-30 12:15:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
+#include "lib.h"
 #include <iostream>
-
+using namespace std;
 int main(){
-  return 0;
+    char a = 0;
+    cin >> a;
+    if (funzione(a) == false) {
+        cout << "errore" << endl;
+    } else {
+        int conv=(int) a;
+        if (97 <= a) {
+            conv = toupper(a);
+            cout << char(conv) << endl;
+        } else if (65 <= conv <= 90) {
+            cout << char(tolower(a));
+        }
+    }
+    return 0;
 }
